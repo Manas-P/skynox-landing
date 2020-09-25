d15058a7044d7505ddfd2d2ba7d6aea0976a44e2
Manas P <manas4518pachakkil@gmail.com>
2020-09-26 03:24:22 +0530

    Navbar Completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+//Hero Animation
 var animation = bodymovin.loadAnimation({
   container: document.getElementById("hero"),
   renderer: "svg",
@@ -5,3 +6,17 @@ var animation = bodymovin.loadAnimation({
   autoplay: true,
   path: "./assets/animations/heroAnimation.json",
 });
+
+//Header Sroll Active
+const header = document.querySelector("header");
+const compName = document.querySelector(".compName");
+window.onscroll = function () {
+  var top = window.scrollY;
+  if (top >= 200) {
+    header.classList.add("headerActive");
+    compName.classList.add("compNameAct");
+  } else {
+    header.classList.remove("headerActive");
+    compName.classList.remove("compNameAct");
+  }
+};
